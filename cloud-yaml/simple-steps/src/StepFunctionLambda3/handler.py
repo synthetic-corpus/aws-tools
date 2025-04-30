8c174bdadf8967b84b8037a8cfa2fb9019df71b3
Joel G <joel@joelgonzaga.com>
2025-04-30 13:26:19 -0700

    created a working stack for step functions.

---
+++ cloud-yaml/simple-steps/src/StepFunctionLambda3/handler.py
@@ -0,0 +1,9 @@
+import os
+
+def lambda_handler(event, context):
+    env = os.getenv("ENVIRONMENT", "undefined")
+    input_str = event.get("inputString", "")
+    return {
+        "environment": env,
+        "output": input_str + "_Step3"
+    }
