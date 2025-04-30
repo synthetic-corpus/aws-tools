a655911005b33bc219464a0c1a37fb453da90f1d
Joel G <joel@joelgonzaga.com>
2025-04-30 14:16:00 -0700

    step machine works.

--- cloud-yaml/simple-steps/src/StepFunctionLambda2/handler.py
+++ cloud-yaml/simple-steps/src/StepFunctionLambda2/handler.py
@@ -2,7 +2,7 @@ import os
 
 def lambda_handler(event, context):
     env = os.getenv("ENVIRONMENT", "undefined")
-    input_str = event.get("inputString", "")
+    input_str = event.get("output", "")
     return {
         "environment": env,
         "output": input_str + "_Step2"
